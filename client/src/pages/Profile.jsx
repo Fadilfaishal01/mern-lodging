f31bdae107bd214b11a35aaa6b7a8b5df23163fd
Fadilfaishal01 <pudgelovers1st@gmail.com>
2024-02-02 15:55:28 +0700

    Add page profile & session

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,14 @@
-import { FaSignOutAlt } from "react-icons/fa";
-import { useState } from "react";
+import {
+	FaSignOutAlt,
+	FaTrashAlt,
+	FaPlusCircle,
+	FaPencilAlt,
+} from "react-icons/fa";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 const Profile = () => {
+	const fileRef = useRef(null);
 	const { currentUser } = useSelector((state) => state.auth);
 	const [formData, setFormData] = useState({
 		username: currentUser.username || "",
@@ -12,9 +18,11 @@ const Profile = () => {
 
 	return (
 		<div className='p-3 max-w-lg mx-auto'>
-			<h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+			<h1 className='text-3xl font-semibold text-center my-4'>Profile</h1>
 			<form className='flex flex-col gap-3'>
+				<input type='file' ref={fileRef} hidden accept='image/*' />
 				<img
+					onClick={() => fileRef.current.click()}
 					className='rounded-full h-24 w-24 object-cover cursor-pointer self-center my-2'
 					src={currentUser.avatar}
 					alt={`Profile User ${currentUser.name}`}
@@ -39,20 +47,23 @@ const Profile = () => {
 					id='password'
 					className='border p-3 rounded-lg'
 				/>
-				<button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-85 disabled:opacity-80'>
-					Update
+				<button className='bg-slate-700 text-white rounded-lg p-3 hover:opacity-85 disabled:opacity-80 flex items-center justify-center gap-2'>
+					<FaPencilAlt />
+					<span className='font-semibold'>Update</span>
 				</button>
-				<button className='bg-green-800 text-white rounded-lg p-3 uppercase hover:opacity-85 disabled:opacity-80'>
-					Create Listing
+				<button className='bg-green-800 text-white rounded-lg p-3 hover:opacity-85 disabled:opacity-80 flex items-center justify-center gap-2'>
+					<FaPlusCircle />
+					<span className='font-semibold'>Create Listing</span>
 				</button>
 			</form>
-			<div>
-				<button className='bg-yellow-700 text-white p-3 hover:opacity-80'>
-					<FaSignOutAlt />
-					<span>Delete Account</span>
+			<div className='flex justify-between mt-3'>
+				<button className='bg-yellow-500 rounded-lg text-white p-2 hover:opacity-80 flex items-center justify-center gap-2'>
+					<FaTrashAlt />
+					<span className='font-semibold'>Delete Account</span>
 				</button>
-				<button className='bg-red-700 text-white p-3 hover:opacity-80'>
-					Sign Out
+				<button className='bg-red-700 rounded-lg text-white p-3 hover:opacity-80 flex items-center justify-center gap-2'>
+					<FaSignOutAlt />
+					<span className='font-semibold'>Sign Out</span>
 				</button>
 			</div>
 		</div>
